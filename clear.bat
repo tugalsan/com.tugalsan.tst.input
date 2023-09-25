c6ea07af1a08f9c66603aea2675cf57f8bd2b8a5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-26 01:49:26 +0300

    .

--- clear.bat
+++ clear.bat
@@ -1,4 +1,5 @@
 SET F=D:\git\gvm\com.tugalsan.tst.input\target
 IF EXIST %F% RMDIR /S /Q %F%
 
-del com.tugalsan.tst.input-1.0-SNAPSHOT-jar-with-dependencies.exe
+del com.tugalsan.tst.input.exe
+del *.dll
--- src/main/java/com/tugalsan/tst/input/Main.java
+++ src/main/java/com/tugalsan/tst/input/Main.java
@@ -1,9 +1,11 @@
 package com.tugalsan.tst.input;
 
 import java.awt.AWTException;
+import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import static java.lang.System.out;
@@ -18,14 +20,28 @@ public class Main {
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.input-1.0-SNAPSHOT-jar-with-dependencies.jar
     //java -jar target/com.tugalsan.tst.input-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String... s) {
-        var size = size();
-        log.ci("main", "size", size);
-        var img = shotPicture(size);
+
+        var screenRectangle = screenRectangle();
+        log.ci("main", "screenRectangle", screenRectangle);
+        var scaleFactor = scaleFactor();
+        log.ci("main", "scaleFactor", scaleFactor);
+        screenRectangle.setRect(
+                scaleFactor * screenRectangle.x,
+                scaleFactor * screenRectangle.y,
+                scaleFactor * screenRectangle.width,
+                scaleFactor * screenRectangle.height
+        );
+
+        var img = shotPicture(screenRectangle);
         if (img == null) {
             return;
         }
         var jpg = Path.of("D:\\git\\tst\\com.tugalsan.tst.input\\out.jpg");
         toJpg(img, jpg);
+    }
+
+    static float scaleFactor() {
+        return Toolkit.getDefaultToolkit().getScreenResolution() / 96f;
     }
 
     static void toJpg(BufferedImage input, Path output) {
@@ -36,16 +52,12 @@ public class Main {
         }
     }
 
-    static Rectangle size() {
-        var localGrapicsEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        var screensDevices = localGrapicsEnv.getScreenDevices();
-        var allScreenBounds = new java.awt.Rectangle();
-        for (var screen : screensDevices) {
-            var screenBounds = screen.getDefaultConfiguration().getBounds();
-            allScreenBounds.width += screenBounds.width;
-            allScreenBounds.height = Math.max(allScreenBounds.height, screenBounds.height);
+    static Rectangle screenRectangle() {
+        var rectangle = new Rectangle(0, 0, 0, 0);
+        for (var graphicsDevice : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
+            rectangle = rectangle.union(graphicsDevice.getDefaultConfiguration().getBounds());
         }
-        return allScreenBounds;
+        return rectangle;
     }
 
     static BufferedImage shotPicture(Rectangle size) {
