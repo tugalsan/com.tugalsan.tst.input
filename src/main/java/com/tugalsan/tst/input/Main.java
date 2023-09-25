2113cb203394daa33ea316f08e3c9387d1f590ad
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-25 20:16:11 +0300

    .

--- src/main/java/com/tugalsan/tst/input/Main.java
+++ src/main/java/com/tugalsan/tst/input/Main.java
@@ -1,8 +1,9 @@
 package com.tugalsan.tst.input;
 
-import com.tugalsan.api.input.server.TS_InputCommonUtils;
+import java.awt.AWTException;
 import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
+import java.awt.Robot;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import static java.lang.System.out;
@@ -13,18 +14,6 @@ import javax.imageio.ImageIO;
 public class Main {
 
 //    private static final TS_Log d = TS_Log.of(Main.class);
-    static class log {
-
-        public static void ci(String funcName, Object... oAll) {
-            out.print(funcName);
-            Arrays.stream(oAll).forEachOrdered(oi -> {
-                out.print(" ");
-                out.print(oi);
-            });
-            out.println();
-        }
-    }
-
     //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.input
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.input-1.0-SNAPSHOT-jar-with-dependencies.jar
     //java -jar target/com.tugalsan.tst.input-1.0-SNAPSHOT-jar-with-dependencies.jar
@@ -32,6 +21,9 @@ public class Main {
         var size = size();
         log.ci("main", "size", size);
         var img = shotPicture(size);
+        if (img == null) {
+            return;
+        }
         var jpg = Path.of("D:\\git\\tst\\com.tugalsan.tst.input\\out.jpg");
         toJpg(img, jpg);
     }
@@ -57,7 +49,24 @@ public class Main {
     }
 
     static BufferedImage shotPicture(Rectangle size) {
-        return TS_InputCommonUtils.robot().createScreenCapture(size);
+        try {
+            return new Robot().createScreenCapture(size);
+        } catch (AWTException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    static class log {
+
+        public static void ci(String funcName, Object... oAll) {
+            out.print(funcName);
+            Arrays.stream(oAll).forEachOrdered(oi -> {
+                out.print(" ");
+                out.print(oi);
+            });
+            out.println();
+        }
     }
 
 }
